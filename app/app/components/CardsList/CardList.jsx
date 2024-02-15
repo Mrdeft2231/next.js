3fe95547de7ec9fb499a6b798fd632192f0328b1
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-02-16 01:27:53 +0300

    add

--- app/app/components/CardsList/CardList.jsx
+++ app/app/components/CardsList/CardList.jsx
@@ -1,16 +1,24 @@
 import Styles from './CardsList.module.css'
 import {PopularCardsFragment} from './PopularCardsFragment' 
 import { NewCardsFragment } from './NewCardsFragment'
+import { Card } from '../Card/Card';
 export const CardList = (props) => {
   return (
     <section className={Styles['list-section']}>
     <h2 className={Styles['list-section__title']} id={props.id}>
       {props.title}
     </h2>
-    <ul className={Styles['cards-list']}>
-      {props.id === "popular" && <PopularCardsFragment/>}
-      {props.id === "new" && <NewCardsFragment/>}
-    </ul>
+    <ul className={Styles["cards-list"]}>
+    {props.data.map((item)  => {
+  return (
+    <li className={Styles["card-list__item"]} key={item.id}>
+      <a href={item.link} target='_blank' className={Styles["card-list__link"]}>
+        <Card {...item} />
+      </a>
+    </li>
+  ); 
+  })}
+  </ul>
   </section>
   )
 }
--- app/app/page.js
+++ app/app/page.js
@@ -1,18 +1,15 @@
-import { NewCardsFragment } from './components/CardsList/NewCardsFragment'
-import { PopularCardsFragment } from './components/CardsList/PopularCardsFragment'
+import {getGamesByCategory} from './data/data-utils';
 import { CardList } from './components/CardsList/CardList'
 import { Promo } from './components/Promo/Promo'
 import { Banner } from './components/Banner/Banner'
-import Image from "next/image";
-import styles from "./page.module.css";
-export default function Home(props) {
+export default function Home() {
+  const popularGames = getGamesByCategory("popular");
+  const newGames = getGamesByCategory("new");
   return (
     <main className="main" >
       <Banner/>
-      <CardList id="popular" title="Популярные">
-      </CardList>
-      <CardList id="new" title="Новинки">
-      </CardList>
+      <CardList id="popular" title="Популярные" data={popularGames}/>
+      <CardList id="new" title="Новинки" data={newGames}/>
       <Promo/>
   </main>
   );
