94ed57836c9fe46758fa9a1fdb005108ec758f65
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-03-08 18:41:30 +0300

    add

---
+++ app/app/api/api-utils.js
@@ -0,0 +1,29 @@
+export const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data
+}
+
+const normalizeDataObject = (obj) => {
+  return {
+    ...obj,
+    category: obj.categories,
+    users: obj.users_permissions_users,
+  }
+}
+
+export const normalizeData = (data) => {
+  return data.map((item) => {
+    return normalizeDataObject(item)
+  })
+}
+
+export const getNormalizedGameDataById = async (url, id) => {
+  const data = await getData(`${url}/${id}`);
+  return normalizeDataObject(data)
+}
+
+export const getNormalizedGamesDataByCategory = async (url, category) => {
+  const data = await getData(`${url}?category=${category}`);
+  return normalizeData(data)
+}
--- app/app/popular/page.js
+++ app/app/popular/page.js
@@ -1,8 +1,9 @@
 import { getGamesByCategory } from '../data/data-utils.js';
 import {CardList} from '../components/CardsList/CardList';
+import { getNormalizedGamesDataByCategory } from '../api/api-utils.js'
 
-export default function Popular() {
-  const popularGames = getGamesByCategory("popular");
+export default async function Popular() {
+  const popularGames = getNormalizedGamesDataByCategory(endpoints.games, 'popular');
   return (
     <main className={"main-inner"}>
       <CardList id="popular" title="Популярные" data={popularGames} />
